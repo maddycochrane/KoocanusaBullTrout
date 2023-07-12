2837a5f23423ccec624bf272008ac319e071e8c7
Maddy Cochrane <46608977+maddycochrane@users.noreply.github.com>
2023-07-12 10:11:31 -0600

    Update Koocanusa.R

--- Koocanusa.R
+++ Koocanusa.R
@@ -1,5 +1,6 @@
 
 
+
 ##### Bull Trout and Kokanee Data from Koocanusa System 1975 - 2022 ######
 ### Visually exploring data and trends 
 
@@ -108,9 +109,9 @@ p1<-ggplot(data=comb.dat)+
 p1
 
 # save figure
-tiff("Results/bull.kok.cpue.tiff",width = 6, height = 6, units = 'in', res = 300)
-p1
-dev.off()
+#tiff("Results/bull.kok.cpue.tiff",width = 6, height = 6, units = 'in', res = 300)
+#p1
+#dev.off()
 
 
 
@@ -134,9 +135,9 @@ p2<-ggplot(data=juv.bull.dat)+
 p2
 
 # save figure
-tiff("Results/juv.bull.pop.tiff",width = 6, height = 6, units = 'in', res = 300)
-p2
-dev.off()
+#tiff("Results/juv.bull.pop.tiff",width = 6, height = 6, units = 'in', res = 300)
+#p2
+#dev.off()
 
 
 # only site above libby dam
@@ -151,9 +152,9 @@ grave<-juv.bull.dat %>% filter(Site == "GraveCreek")%>%
 grave
 
 # save figure
-tiff("Results/graveCreek.juv.bull.pop.tiff",width = 6, height = 6, units = 'in', res = 300)
-grave
-dev.off()
+#tiff("Results/graveCreek.juv.bull.pop.tiff",width = 6, height = 6, units = 'in', res = 300)
+#grave
+#dev.off()
 
 
 
@@ -164,7 +165,7 @@ str(kok.spawn.counts)
 
 # go from wide to long 
 long.kok.spawn<-kok.spawn.counts%>%
-  gather(key=Site, value= Count, SandCreek:Total, factor_key=TRUE)
+  gather(key=Site, value= Count, SandCreek:AvgPerSurvey, factor_key=TRUE)
 head(long.kok.spawn)
 
 # quick view of kokanee spawning data
@@ -176,18 +177,20 @@ ggplot(data=long.kok.spawn)+
   #scale_color_grey()+
   theme_classic()
 
-p3<-ggplot(data=long.kok.spawn)+
+p3<-long.kok.spawn %>% filter(Site != "Total")%>%
+ggplot()+
   geom_point(aes(x=Year,y=Count))+
-  geom_line(aes(x=Year,y=Count))+
+  geom_line(aes(x=Year,y=Count),alpha=0.4)+
   labs(y="Kokanee Escapement")+
-  scale_color_grey()+theme_classic()+facet_wrap(~Site,nrow=2)
+  scale_color_grey()+theme_classic()+facet_wrap(~Site,nrow=2, scales = "free_y")+
+  scale_x_continuous(breaks=c(2000,2005,2010,2015, 2020), labels=c('00', '05','10', '15', '20'))
 p3
 
 
 # save figure
-tiff("Results/kok.spawn.tiff",width = 10, height = 6, units = 'in', res = 300)
-p3
-dev.off()
+#tiff("Results/kok.spawn.tiff",width = 10, height = 6, units = 'in', res = 300)
+#p3
+#dev.off()
 
 
 
@@ -200,27 +203,45 @@ str(bt.redd.counts)
 
 bt1<-bt.redd.counts %>%
   filter(CoreArea == "Lake Koocanusa")%>% # not including Kootenai River
-  filter(LocalPopulation != "Wigwam River")%>%
+  filter(Waterbody != "Wigwam River Canada")%>%
+  filter(LocalPopulation != "Blackfoot Creek")%>% # data from 17 + has 3 entries
 ggplot()+
-  geom_point(aes(x=Year,y=Redds,color=LocalPopulation))+
-  geom_line(aes(x=Year,y=Redds,color=LocalPopulation),alpha=0.2)+
+  geom_point(aes(x=Year,y=Redds,color=LocalPopulation,group=Waterbody))+
+  geom_line(aes(x=Year,y=Redds,color=LocalPopulation,group=Waterbody),alpha=0.3)+
   labs(y="Bull Trout Redd Count")+
   #scale_color_grey()+
   theme_classic()+ 
   scale_y_continuous(expand = c(0, 0)) # forcing y axis to start at 0
 bt1
 
+# view with facet wrap
+redd.fig<-bt.redd.counts %>%
+  filter(CoreArea == "Lake Koocanusa")%>% # not including Kootenai River
+  #filter(Waterbody != "Wigwam River Canada")%>%
+  ggplot()+
+  geom_point(aes(x=Year,y=Redds,color=Waterbody))+
+  geom_line(aes(x=Year,y=Redds,color=Waterbody),alpha=0.5)+
+  labs(y="Bull Trout Redd Count")+
+  #scale_color_grey()+
+  theme_classic()+ 
+  scale_y_continuous(expand = c(0, 0))+ # forcing y axis to start at 0
+  facet_wrap(~LocalPopulation, scales = "free_y")
+redd.fig
+
 bt2<-bt.redd.counts %>%
   filter(CoreArea == "Lake Koocanusa")%>% # not including Kootenai River
-  filter(LocalPopulation == "Wigwam River")%>%
+  filter(Waterbody == "Wigwam River Canada")%>%
   ggplot()+
-  geom_point(aes(x=Year,y=Redds,color=LocalPopulation))+
-  geom_line(aes(x=Year,y=Redds,color=LocalPopulation),alpha=0.2)+
+  geom_point(aes(x=Year,y=Redds,color=Waterbody,group=Waterbody))+
+  geom_line(aes(x=Year,y=Redds,color=Waterbody,group=Waterbody),alpha=0.3)+
+  scale_color_manual(values=c("#619CFF"))+
   labs(y="Bull Trout Redd Count")+
   #scale_color_grey()+
   theme_classic()+ 
   scale_y_continuous(expand = c(0, 0)) # forcing y axis to start at 0
 bt2
+
+
 
 duplot<-cowplot::plot_grid(bt1 +
                              theme(axis.title.x = element_blank()),
@@ -232,6 +253,73 @@ duplot<-cowplot::plot_grid(bt1 +
 duplot
 
 # save figure
-tiff("Results/bt.redd.counts.tiff",width = 6, height = 6, units = 'in', res = 300)
-duplot
-dev.off()
+#tiff("Results/bt.redd.counts.tiff",width = 10, height = 6, units = 'in', res = 300)
+#redd.fig
+#dev.off()
+
+
+
+
+
+#########################################################
+############ fitting a stock recruitment curve #########
+
+# x = stock level / spawners 
+# y = recruitment level / juveniles 
+
+# juvenile data are age 1+ but not migratory adults > 75 mm
+# thus we need to lag the redd counts by 2 yrs 
+
+
+#### grave creek 
+
+# juvenile bull trout data 
+head(juv.bull.dat)
+grave.juv.bull.dat <- juv.bull.dat %>% filter(Site=="GraveCreek") # grave creek only 
+
+# bt redd data 
+head(bt.redd.counts)
+grave.bt.redd.dat<- bt.redd.counts %>% filter(LocalPopulation == "Grave Creek")
+
+# combining redd counts across grave creek tribs into one total value 
+grave.bt.redd.tot <- grave.bt.redd.dat %>% 
+  group_by(LocalPopulation,Year)%>%
+  summarise(sum(Redds))%>%
+  rename(TotalRedds = `sum(Redds)`)%>%
+  arrange(Year)%>%
+  mutate(lagRedds = lag(TotalRedds, n=2))%>%
+  dplyr::select(Year,lagRedds)
+
+# join juv and redd data 
+head(grave.bt.redd.tot)
+head(grave.juv.bull.dat)
+
+grave.sr<-left_join(grave.juv.bull.dat, grave.bt.redd.tot, by="Year")
+head(grave.sr)
+
+grave.sr<-ggplot(data=grave.sr,aes(x=lagRedds, y=log(PopEst/lagRedds )))+
+  geom_point()+
+  labs(x="Redd Count (2 yr lag)", y="log(Juvenile Population Est. / Redd Count)",
+       title="Grave Creek Stock-Recruit Function")+
+  geom_smooth(data=grave.sr, aes(x=lagRedds, y=log(PopEst/lagRedds )),
+              method = lm, formula = y ~ x)+
+  ggpmisc::stat_poly_eq(ggpmisc::use_label(c("eq","R2","p")), formula = y ~ x)
+grave.sr
+# save image
+#ggsave("Results/grave.sr.fnct.png",width = 4, height =4)
+
+# linearized Ricker model 
+mod<- lm(log(PopEst/lagRedds) ~ lagRedds, data = grave.sr) 
+summary(mod)
+# beta = -0.009735 , p < 0.001, F stat = 55.33
+
+ggplot(data=grave.sr,aes(x=Year, y=log(PopEst/lagRedds )))+
+         geom_point()+
+  labs(y="log(Juvenile Population Est. / Redd Count)")
+# save image
+ggsave("Results/sr.time.png",width = 4, height =4)
+
+
+
+
+
